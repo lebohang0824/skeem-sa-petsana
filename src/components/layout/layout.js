3110828e3aa44fd8947afda3b1a39d21d370ffef
Lebohang <lebohang0824@gmail.com>
2020-09-09 17:39:39 +0200

    Change layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -7,25 +7,11 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import "./layout.scss"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
@@ -35,9 +21,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Build for
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://skeemsapetsana.co.za/">SkeemSaPetsana</a>
         </footer>
       </div>
     </>
